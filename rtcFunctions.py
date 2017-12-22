253f63ea57d24169d5cb7159935fa74c0b36b317
zevanty <95801+zevanty@users.noreply.github.com>
2017-12-22 15:17:49 -0800

    RTC login shouldn't use quotes. Exit if login failed.

--- rtcFunctions.py
+++ rtcFunctions.py
@@ -28,7 +28,9 @@ class RTCLogin:
     @staticmethod
     def loginandcollectstreamuuid():
         config = configuration.get()
-        shell.execute("%s login -r %s -u '%s' -P '%s'" % (config.scmcommand, config.repo, config.user, config.password))
+        exitcode = shell.execute("%s login -r %s -u %s -P %s" % (config.scmcommand, config.repo, config.user, config.password))
+        if exitcode is not 0:
+            sys.exit("Login failed. Please check your connection and credentials.")
         config.collectstreamuuids()
 
     @staticmethod
@@ -187,7 +189,7 @@ class ImportHandler:
         for entry in componentbaselinesentries:
             shouter.shout("Determine initial baseline of " + entry.componentname)
             # use always scm, lscm fails when specifying maximum over 10k
-            command = "scm --show-alias n --show-uuid y list baselines --components %s -r %s -u %s -P '%s' -m 20000" % \
+            command = "scm --show-alias n --show-uuid y list baselines --components %s -r %s -u %s -P %s -m 20000" % \
                       (entry.component, config.repo, config.user, config.password)
             baselineslines = shell.getoutput(command)
             baselineslines.reverse()  # reverse to have earliest baseline on top
